20a86b77a7b9142b90bd7a1d7f70f5256d9b2c46
Santhosh Kumar <santhoshkumar@Apples-MacBook-Pro.local>
2020-12-16 11:11:43 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Template1 from './component/Templates/Template1/Template1';
+
+function App() {
+  return (
+    <div className="App">
+      <Template1/>
+    </div>
+  );
+}
+
+export default App;
